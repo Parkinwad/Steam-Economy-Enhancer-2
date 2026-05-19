0cd04167315c002cd8a5f634de01ff539f122c9e
Parkinwad <parkinwad@gmail.com>
2026-05-18 22:58:15 -0400

    Many code bug fixes. Prob still more to go.

--- .continue/rules/edit-code-rules.md
+++ .continue/rules/edit-code-rules.md
@@ -5,3 +5,4 @@ description: Code writing and editing rules.
 Rule 1: Optimize Code for Efficiency
 Rule 2: Do not delete code unless the deletion is part of the edit. If unsure, ask before deletion to get confirmation.
 Rule 3: Check all newly written code for syntax and formatting errors before running the edit or apply after creating a change.
+Rule 4: Refer to merge_final.js for syntax on how to apply any code changes correctly as this was a working javascript file for a code edit / merge.
---
+++ replace.ps1
@@ -0,0 +1,27 @@
+﻿ = 'code.user.js'
+ = Get-Content  -Raw
+ = '(?s)(    SteamMarket\.prototype\.getPriceBeforeFeesForDesiredReceivedAmount = function \(desiredReceivedAmount\) \{[\s\S]*?return low;\s*\};)'
+ = @'
+    SteamMarket.prototype.getPriceBeforeFeesForDesiredReceivedAmount = function (desiredReceivedAmount) {
+        let low = desiredReceivedAmount;
+        let high = desiredReceivedAmount * 2;
+        
+        while (low <= high) {
+            const mid = Math.floor((low + high) / 2);
+            const receivedPrice = this.getPriceIncludingFees(mid);
+            
+            if (receivedPrice < desiredReceivedAmount) {
+                low = mid + 1;
+            } else if (receivedPrice > desiredReceivedAmount) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return low;
+    };
+'@
+ = [regex]::Replace(, , , 'Singleline')
+Set-Content  -Value  -NoNewline
+Write-Host "Replacement complete"
