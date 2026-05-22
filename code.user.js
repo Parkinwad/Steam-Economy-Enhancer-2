92cebd70092ec728ee6c7839cbc3088460311341
Parkinwad <parkinwad@gmail.com>
2026-05-22 14:00:35 -0400

    Should have persistent deal scanner settings now

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Parkinwad
 // @author       Parkinwad
 // @license      MIT
-// @version      0.1.12
+// @version      0.1.13
 // @description  Enhances the Steam Inventory and Steam Market.
 // @match        https://steamcommunity.com/id/*/inventory*
 // @match        https://steamcommunity.com/profiles/*/inventory*
@@ -4338,6 +4338,17 @@
 
     //#region Settings
     function openSettings() {
+        // Save default values to localStorage on first open (if not already present)
+        const dealScannerSettings = ['SETTING_DEAL_SCANNER_ENABLED', 'SETTING_DEAL_MIN_PROFIT_CENTS', 'SETTING_DEAL_BOOSTER_MIN_PROFIT_CENTS', 'SETTING_DEAL_MAX_ITEMS_PER_SCAN', 'SETTING_DEAL_MAX_SPEND_PER_SCAN','SETTING_DEAL_SCAN_DELAY_MS'];
+
+        for (const setting of dealScannerSettings) {
+            const storedValue = getLocalStorageItem(setting);
+            if (storedValue === null) {
+                // First time opening - save default value
+                setSetting(setting, settingDefaults[setting]);
+            }
+        }
+
         const price_options = $(`<div id="see_settings_modal">
             <div>
                 Calculate prices as the:&nbsp;
